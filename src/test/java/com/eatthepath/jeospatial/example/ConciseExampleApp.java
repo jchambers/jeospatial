d1c939ec50d27dde43c9c40aa40c470dd229e7e8
Jon Chambers <jon@relayrides.com>
2015-06-23 14:56:51 -0400

    Un-hosed the concise example app.

--- src/test/java/com/eatthepath/jeospatial/example/ConciseExampleApp.java
+++ src/test/java/com/eatthepath/jeospatial/example/ConciseExampleApp.java
@@ -1,58 +1,60 @@
 package com.eatthepath.jeospatial.example;
 
 import java.io.IOException;
+import java.util.List;
+
+import com.eatthepath.jeospatial.GeospatialIndex;
+import com.eatthepath.jeospatial.SimpleGeospatialPoint;
+import com.eatthepath.jeospatial.VPTreeGeospatialPointIndex;
 
 /**
  * A very simple test application that shows the very basics of using a
  * geospatial point database.
- * 
+ *
  * @author <a href="mailto:jon.chambers@gmail.com">Jon Chambers</a>
  */
 public class ConciseExampleApp {
     public static void main(String[] args) throws IOException {
-        /* ConciseExampleApp.getTenClosestNeighbors();
+        ConciseExampleApp.getTenClosestNeighbors();
         ConciseExampleApp.getAllWithinRange();
-        ConciseExampleApp.getAllInBoundingBox(); */
+        ConciseExampleApp.getAllInBoundingBox();
     }
 
-    /* public static void getTenClosestNeighbors() throws IOException {
-        // Load a bunch of zip codes from a file and construct a vp-tree from
-        // those points
-        List<ZipCode> zipCodes = ZipCode.loadAllFromCsvFile();
-        GeospatialIndex<ZipCode> database = new VPTree<ZipCode>(zipCodes);
+    public static void getTenClosestNeighbors() throws IOException {
+        // Load a bunch of zip codes from a file and construct a vp-tree from those points
+        List<ZipCode> zipCodes = ZipCodeLoader.loadAllZipCodes();
+        GeospatialIndex<ZipCode> index = new VPTreeGeospatialPointIndex<ZipCode>(zipCodes);
 
         // Pick a query point (Davis Square in Somerville, MA, USA)
         SimpleGeospatialPoint davisSquare = new SimpleGeospatialPoint(42.396745, -71.122479);
 
         // Find the ten nearest zip codes to Davis Square
-        List<ZipCode> nearestZipCodes = database.getNearestNeighbors(davisSquare, 10);
+        List<ZipCode> nearestZipCodes = index.getNearestNeighbors(davisSquare, 10);
 
         ConciseExampleApp.printZipCodeList("Ten nearest zip codes to Davis Square:", nearestZipCodes);
     }
 
     public static void getAllWithinRange() throws IOException {
-        // Load a bunch of zip codes from a file and construct a vp-tree from
-        // those points
-        List<ZipCode> zipCodes = ZipCode.loadAllFromCsvFile();
-        GeospatialIndex<ZipCode> database = new VPTree<ZipCode>(zipCodes);
+        // Load a bunch of zip codes from a file and construct a vp-tree from those points
+        List<ZipCode> zipCodes = ZipCodeLoader.loadAllZipCodes();
+        GeospatialIndex<ZipCode> index = new VPTreeGeospatialPointIndex<ZipCode>(zipCodes);
 
         // Pick a query point (Davis Square in Somerville, MA, USA)
         SimpleGeospatialPoint davisSquare = new SimpleGeospatialPoint(42.396745, -71.122479);
 
         // Find all zip codes within ten kilometers of Davis Square
-        List<ZipCode> zipCodesWithinRange = database.getAllNeighborsWithinDistance(davisSquare, 10 * 1000);
+        List<ZipCode> zipCodesWithinRange = index.getAllWithinRange(davisSquare, 10e3);
 
         ConciseExampleApp.printZipCodeList("All zip codes within 10km of Davis Square:", zipCodesWithinRange);
     }
 
     public static void getAllInBoundingBox() throws IOException {
-        // Load a bunch of zip codes from a file and construct a vp-tree from
-        // those points
-        List<ZipCode> zipCodes = ZipCode.loadAllFromCsvFile();
-        GeospatialIndex<ZipCode> database = new VPTree<ZipCode>(zipCodes);
+        // Load a bunch of zip codes from a file and construct a vp-tree from those points
+        List<ZipCode> zipCodes = ZipCodeLoader.loadAllZipCodes();
+        GeospatialIndex<ZipCode> index = new VPTreeGeospatialPointIndex<ZipCode>(zipCodes);
 
         // Find all of the zip codes in a bounding "box"
-        List<ZipCode> inBoundingBox = database.getAllPointsInBoundingBox(-75, -70, 43, 42);
+        List<ZipCode> inBoundingBox = index.getAllPointsInBoundingBox(-75, -70, 43, 42);
 
         ConciseExampleApp.printZipCodeList("Zip codes inside bounding box:", inBoundingBox);
     }
@@ -63,5 +65,5 @@ public class ConciseExampleApp {
         for(ZipCode z : zipCodes) {
             System.out.format("\t%s%n", z);
         }
-    } */
+    }
 }
